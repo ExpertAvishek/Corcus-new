3b78235f13e3d55a946ad28dd27179c53c5762c0
Your Name Here <your@email.com>
2025-03-26 12:32:08 +0530

    aaru

--- src/Pages/ContactPage.jsx
+++ src/Pages/ContactPage.jsx
@@ -1,11 +1,13 @@
 import BreadCumb from "../Components/Common/BreadCumb";
 import ContactInfo2 from "../Components/ContactInfo/ContactInfo2";
+import Contacthero from "../Components/HeroBanner/Contacthero";
+
 
 
 const ContactPage = () => {
     return (
         <div>
-             
+            <Contacthero></Contacthero>
             <ContactInfo2></ContactInfo2>            
             
         </div>

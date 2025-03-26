3e12599fde934e89857a71ec441a619b89d657b6
Your Name Here <your@email.com>
2025-03-26 12:32:15 +0530

    aaru

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -1,19 +1,18 @@
-import About2 from "../Components/About/About2";
-import BreadCumb from "../Components/Common/BreadCumb";
 import Counter3 from "../Components/Counter/Counter3";
-import Team2 from "../Components/Team/Team2";
-import Testimonial3 from "../Components/Testimonial/Testimonial3";
 import Value1 from "../Components/Value/Value1";
+import Abouthero from "../Components/HeroBanner/Abouthero";
+import ContactPage from "./ContactPage";
+import Team2 from "../Components/Team/Team2";
 
 
 const AboutPage = () => {
     return (
         <div> 
-            <About2 addclass="about-section-2 fix section-padding"></About2>
+            <Abouthero></Abouthero>
             <Counter3></Counter3>
             <Value1></Value1>
             <Team2></Team2>
-            <Testimonial3></Testimonial3>
+            <ContactPage></ContactPage>
         </div>
     );
 };
